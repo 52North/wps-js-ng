f0a0bc61dbca3919ac151e23efa00d5977d0093d
Karan Singh <karan100194@gmail.com>
2020-06-09 10:59:12 -0400

    Creating WPSService Typescript class. Adding Demo Html

--- src/app/wps-example/wps-example.component.ts
+++ src/app/wps-example/wps-example.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {WpsServiceTs} from '../wps-service-ts';
+import {Settings} from '../core/settings';
+import {CapabilitiesResponse} from '../core/model/capabilities/capabilities-response';
 
 @Component({
   selector: 'app-wps-example',
   templateUrl: './wps-example.component.html',
   styleUrls: ['./wps-example.component.css']
 })
-export class WpsExampleComponent implements OnInit {
 
-  constructor() { }
+
+export class WpsExampleComponent implements OnInit {
+  private static WPS_VERSION_1 = '1.0.0';
+  private static WPS_VERSION_2 = '2.0.0';
+  private wpsServiceJS: any;
+  title: string;
+
+  getCapabilitiesGet(callback: (capabilitiesResponse: CapabilitiesResponse) => void) {
+  }
 
   ngOnInit(): void {
   }
-
 }
---
+++ src/app/wps-service-ts.ts
@@ -0,0 +1,5 @@
+export class WpsServiceTs {
+  serviceInject(): string{
+    return 'Wps Service Inject';
+  }
+}
