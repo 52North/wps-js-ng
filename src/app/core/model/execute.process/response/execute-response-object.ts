2e76cce079ebd26bebb5fcb58e8f7ecdf2982931
Karan Singh <karan100194@gmail.com>
2020-07-15 01:11:47 -0400

    Adding execute response object

---
+++ src/app/core/model/execute.process/response/execute-response-object.ts
@@ -0,0 +1,26 @@
+import {ExecuteResponseDocument} from './execute-response-document';
+
+export class ExecuteResponseObject {
+  private readonly _type: string;
+  private readonly _serviceVersion: string;
+  private readonly _responseDocument: ExecuteResponseDocument;
+
+  constructor(executeResponseJson: any) {
+    // type: string, serviceVersion: string, responseDocument: ExecuteResponseDocument
+    this._type = executeResponseJson.type;
+    this._serviceVersion = executeResponseJson.serviceVersion;
+    this._responseDocument = new ExecuteResponseDocument(executeResponseJson.responseDocument);
+  }
+
+  get type(): string {
+    return this._type;
+  }
+
+  get serviceVersion(): string {
+    return this._serviceVersion;
+  }
+
+  get responseDocument(): ExecuteResponseDocument {
+    return this._responseDocument;
+  }
+}
