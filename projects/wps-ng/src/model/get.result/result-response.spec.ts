6c1cfc8abf68c4cd945ba2ec63fee9bf6da5f983
Karan Singh <karan100194@gmail.com>
2020-07-26 01:03:34 -0400

    adding spec files and test cases

---
+++ projects/wps-ng/src/model/get.result/result-response.spec.ts
@@ -0,0 +1,7 @@
+import { ResultResponse } from './result-response';
+
+describe('Status', () => {
+  it('should create an instance', () => {
+    expect(new ResultResponse(undefined)).toBeTruthy();
+  });
+});
---
+++ projects/wps-ng/src/model/get.status/status-response-document.spec.ts
@@ -0,0 +1,7 @@
+import {StatusResponseDocument} from './status-response-document';
+
+describe('Status', () => {
+  it('should create an instance', () => {
+    expect(new StatusResponseDocument(undefined)).toBeTruthy();
+  });
+});
