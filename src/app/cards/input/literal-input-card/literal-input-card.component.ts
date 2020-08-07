ebde6e6fdc5b5b8f743ab929f889b43b4e390244
Karan Singh <karan100194@gmail.com>
2020-08-07 11:10:34 -0400

    Updating button titles

--- src/app/cards/input/literal-input-card/literal-input-card.component.ts
+++ src/app/cards/input/literal-input-card/literal-input-card.component.ts
@@ -10,16 +10,13 @@ export class LiteralInputCardComponent implements OnInit {
   @Input() title: string;
   @Input() input: string;
   @Input() literalDataDomains: LiteralDataDomains[];
-  type: LiteralDataDomains[];
+  type = 'Literal Data';
 
   @Output() literalValue: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    if (this.literalDataDomains !== undefined){
-      this.type = this.literalDataDomains;
-    }
   }
 
 }
